0e4539be3938f9354d6181829839f6a282b7f8a9
Ches19 <suharik55x55@gmail.com>
2023-06-05 00:29:18 +0300

    add

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Categories.module.css'
 
 const Categories = props => {
-  const [categories] = useState([
-    {
-      key: 'all',
-      name: 'Всё',
-    },
-    {
-      key: 'keds',
-      name: 'Кеды',
-    },
-    {
-      key: 'kross',
-      name: 'Кроссовки',
-    },
-    {
-      key: 'bots',
-      name: 'Ботинки',
-    },
-  ])
+  const [categories, setCategories] = useState([])
+  const [currentCategories, setCurrentCategories] = useState([])
+
+  useEffect(() => {
+    fetch('https://646f370109ff19b12086caf8.mockapi.io/sh/category')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        const categoriesData = data.map(category => ({ ...category }))
+        setCategories(categoriesData)
+        setCurrentCategories(categoriesData)
+      })
+  }, [])
 
   return (
     <div className={styles.categories}>
-      {categories.map(el => (
+      {currentCategories.map(el => (
         <div
           key={el.key}
-          className={props.selectedCategory === el.key ? 'active' : ''}
+          className={props.chooseCategory === el.key ? styles.active : ''}
           onClick={() => props.chooseCategory(el.key)}
         >
           {el.name}
